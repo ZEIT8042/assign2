06bec4e9322924920459390f0e83d74a6be198d2
ZEIT8042 <p.mccullough@student.unsw.edu.au>
2021-08-31 19:05:06 -0400

    exploitDev

--- app/vuln.c
+++ app/vuln.c
@@ -1,15 +1,35 @@
-//gcc -m32 -fno-stack-protector -no-pie vuln.c -o vuln
-
+//gcc -m32 -fno-stack-protector vuln.c -o vuln
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
 
 void overflow (char* inbuf)
 {
   char buf[4];
+  char str1[] = "A";
   strcpy(buf, inbuf);
+  if (strcmp(str1,inbuf)==0){
+    printf("I am option A\n");
+  }  
+  else {
+  printf("invalid option!!!! Please Enter using capitals\n");
+  }
+  return;
 }
 
-int main (int argc, char** argv)
+
+
+int main (void)
 {
-  overflow(argv[1]);
+  char strInput[64];
+  
+  printf("Enter option between A-B\n");
+  scanf("%s", strInput);
+  printf("You entered: %s\n",strInput);
+  overflow(strInput);
+  
+  
+
   return 0;
 }

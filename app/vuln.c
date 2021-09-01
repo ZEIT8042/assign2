015484cc1c8be193b2673c549cc7804ed1a9d4b8
ZEIT8042 <p.mccullough@student.unsw.edu.au>
2021-08-31 20:33:39 -0400

    execv

--- app/vuln.c
+++ app/vuln.c
@@ -4,13 +4,20 @@
 #include <unistd.h>
 
 
-void overflow (char* inbuf)
+void overflow (char* inbuf, char* argsInput)
 {
   char buf[4];
   char str1[] = "A";
+  int ret=0;
   strcpy(buf, inbuf);
+  printf("Inputs + argument:  %s %s \n",inbuf, argsInput);
   if (strcmp(str1,inbuf)==0){
+    char *binaryPath = "/usr/bin/ls";
+    char *args[] = {binaryPath, argsInput, NULL};
     printf("I am option A\n");
+    printf("I will execute...ls -l");
+    ret=execv(binaryPath,args);
+    printf("done");
   }  
   else {
   printf("invalid option!!!! Please Enter using capitals\n");
@@ -19,15 +26,15 @@ void overflow (char* inbuf)
 }
 
 
-
 int main (void)
 {
   char strInput[64];
+  char strInput2[64];
   
   printf("Enter option between A-B\n");
-  scanf("%s", strInput);
-  printf("You entered: %s\n",strInput);
-  overflow(strInput);
+  scanf("%s %s", strInput, strInput2);
+  printf("You entered: %s %s\n",strInput, strInput2);
+  overflow(strInput, strInput2);
   
   
 
